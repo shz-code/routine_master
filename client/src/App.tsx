2d99a1a39e115a457f75004ca4d6364a9a9f655a
shz-code <web.shahidul.alam@gmail.com>
2024-12-20 20:21:17 +0600

    add: App Layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,14 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<>Hello World</>} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<>Hello World</>} />
+        </Routes>
+      </Layout>
     </Router>
   );
 };
